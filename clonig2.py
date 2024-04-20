884a5f58445933372e2656d37948ee832e951a08
Hackermanking <162139355+Hackermanking@users.noreply.github.com>
2024-04-20 20:20:40 +0300

    Update clonig2.py

--- clonig2.py
+++ clonig2.py
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-if [ -z "https://github.com/Hackermanking/Clonig-fake" ]; then
-    echo "Usage: <https://github.com/Hackermanking/Clonig-fake>"
-    exit 1
-fi
-
-git clone https://github.com/Hackermanking/Clonig-fake
-
-(clonig-fake.git)
-
-cd clonig-fake
+fory ela ra angalatra script
