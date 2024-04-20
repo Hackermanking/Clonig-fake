205875dcbb0c4840b46e7b1fc5b8356661b14ab9
Hackermanking <162139355+Hackermanking@users.noreply.github.com>
2024-04-20 19:42:08 +0300

    Update

---
+++ full.py
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "https://github.com/Hackermanking/Clonig-fake" ]; then
+    echo "Usage: <https://github.com/Hackermanking/Clonig-fake>"
+    exit 1
+fi
+
+git clone https://github.com/Hackermanking/Clonig-fake
+
+(clonig-fake.git)
+
+cd clonig-fake
+
+bash
+#!/bin/bash
+
+
+git clone https://github.com/Hackermanking/Clonig-fake.git
+
+
+cd clonig-fake
+
+
+`Hackermanking``clonig-fake` 
